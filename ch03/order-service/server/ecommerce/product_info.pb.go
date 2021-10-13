ff2ad32dcdb3d2fd174e4891bd016e9c5eae0914
yangjianxun <yangjianxun@topgamesinc.com>
2021-10-13 16:13:16 +0800

    ch03 all

--- ch03/order-service/server/ecommerce/product_info.pb.go
+++ ch03/order-service/server/ecommerce/product_info.pb.go
@@ -96,29 +96,91 @@ func (m *Order) GetDestination() string {
 	return ""
 }
 
+type CombinedShipment struct {
+	Id                   string   `protobuf:"bytes,1,opt,name=id,proto3" json:"id,omitempty"`
+	Status               string   `protobuf:"bytes,2,opt,name=status,proto3" json:"status,omitempty"`
+	OrdersList           []*Order `protobuf:"bytes,3,rep,name=ordersList,proto3" json:"ordersList,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *CombinedShipment) Reset()         { *m = CombinedShipment{} }
+func (m *CombinedShipment) String() string { return proto.CompactTextString(m) }
+func (*CombinedShipment) ProtoMessage()    {}
+func (*CombinedShipment) Descriptor() ([]byte, []int) {
+	return fileDescriptor_9a4d768ec9cb4951, []int{1}
+}
+
+func (m *CombinedShipment) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_CombinedShipment.Unmarshal(m, b)
+}
+func (m *CombinedShipment) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_CombinedShipment.Marshal(b, m, deterministic)
+}
+func (m *CombinedShipment) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_CombinedShipment.Merge(m, src)
+}
+func (m *CombinedShipment) XXX_Size() int {
+	return xxx_messageInfo_CombinedShipment.Size(m)
+}
+func (m *CombinedShipment) XXX_DiscardUnknown() {
+	xxx_messageInfo_CombinedShipment.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_CombinedShipment proto.InternalMessageInfo
+
+func (m *CombinedShipment) GetId() string {
+	if m != nil {
+		return m.Id
+	}
+	return ""
+}
+
+func (m *CombinedShipment) GetStatus() string {
+	if m != nil {
+		return m.Status
+	}
+	return ""
+}
+
+func (m *CombinedShipment) GetOrdersList() []*Order {
+	if m != nil {
+		return m.OrdersList
+	}
+	return nil
+}
+
 func init() {
 	proto.RegisterType((*Order)(nil), "ecommerce.Order")
+	proto.RegisterType((*CombinedShipment)(nil), "ecommerce.CombinedShipment")
 }
 
 func init() { proto.RegisterFile("product_info.proto", fileDescriptor_9a4d768ec9cb4951) }
 
 var fileDescriptor_9a4d768ec9cb4951 = []byte{
-	// 237 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x94, 0x8f, 0xbd, 0x4a, 0x04, 0x31,
-	0x14, 0x85, 0xc9, 0xac, 0x23, 0x4e, 0x14, 0x95, 0x60, 0x31, 0x2c, 0x22, 0x83, 0xd5, 0x54, 0x59,
-	0xd1, 0xce, 0xca, 0x17, 0x10, 0x61, 0x04, 0x5b, 0xc9, 0x26, 0x77, 0xe3, 0x85, 0xcd, 0x0f, 0x37,
-	0x19, 0x7c, 0x04, 0x4b, 0x5f, 0x59, 0x4c, 0xdc, 0xc5, 0x76, 0xcb, 0x73, 0xef, 0xf7, 0xc1, 0x39,
-	0x5c, 0x44, 0x0a, 0x66, 0xd6, 0xf9, 0x1d, 0xfd, 0x26, 0xc8, 0x48, 0x21, 0x07, 0xd1, 0x81, 0x0e,
-	0xce, 0x01, 0x69, 0x58, 0xde, 0xd8, 0x10, 0xec, 0x16, 0x56, 0xe5, 0xb1, 0x9e, 0x37, 0xab, 0x4f,
-	0x52, 0x31, 0x02, 0xa5, 0x8a, 0xde, 0x7e, 0x31, 0xde, 0xbe, 0x90, 0x01, 0x12, 0xe7, 0xbc, 0x41,
-	0xd3, 0xb3, 0x81, 0x8d, 0xdd, 0xd4, 0xa0, 0x11, 0x57, 0xbc, 0xc5, 0x0c, 0x2e, 0xf5, 0xcd, 0xb0,
-	0x18, 0xbb, 0xa9, 0x06, 0x31, 0xf0, 0x53, 0x03, 0x49, 0x13, 0xc6, 0x8c, 0xc1, 0xf7, 0x8b, 0x82,
-	0xff, 0x3f, 0xfd, 0x7a, 0x91, 0x50, 0x43, 0x7f, 0x34, 0xb0, 0xb1, 0x99, 0x6a, 0xf8, 0xf3, 0x32,
-	0x7a, 0x55, 0xbc, 0x76, 0xef, 0xed, 0x4e, 0xf7, 0xdf, 0x8c, 0x5f, 0x94, 0x26, 0xcf, 0xca, 0x2b,
-	0x0b, 0x0e, 0x7c, 0x16, 0x8f, 0xfc, 0xc4, 0x42, 0xae, 0xfd, 0xae, 0x65, 0x9d, 0x22, 0x77, 0x53,
-	0xe4, 0x6b, 0x26, 0xf4, 0xf6, 0x4d, 0x6d, 0x67, 0x58, 0x5e, 0xca, 0xfd, 0x66, 0x59, 0xf9, 0x27,
-	0x7e, 0x96, 0x40, 0x91, 0xfe, 0x28, 0x31, 0x1d, 0xea, 0xdf, 0xb1, 0xf5, 0x71, 0x21, 0x1f, 0x7e,
-	0x02, 0x00, 0x00, 0xff, 0xff, 0xc5, 0x92, 0x86, 0x92, 0x63, 0x01, 0x00, 0x00,
+	// 323 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x94, 0x50, 0x4d, 0x4b, 0x33, 0x31,
+	0x18, 0x24, 0xbb, 0x6f, 0xcb, 0xdb, 0xa7, 0x7e, 0x94, 0x20, 0xb2, 0x54, 0x91, 0xa5, 0xa7, 0x3d,
+	0xa5, 0xa5, 0xde, 0x3c, 0x09, 0x5e, 0xfd, 0x80, 0x2d, 0x78, 0x95, 0x34, 0x79, 0xba, 0x0d, 0x74,
+	0x93, 0x90, 0x64, 0xf1, 0x27, 0xf8, 0x3b, 0xfc, 0xa7, 0xb2, 0xd9, 0xb6, 0x14, 0x0b, 0xa2, 0xc7,
+	0x99, 0xcc, 0x4c, 0xe6, 0x19, 0xa0, 0xd6, 0x19, 0xd9, 0x88, 0xf0, 0xa6, 0xf4, 0xca, 0x30, 0xeb,
+	0x4c, 0x30, 0x74, 0x80, 0xc2, 0xd4, 0x35, 0x3a, 0x81, 0xe3, 0x9b, 0xca, 0x98, 0x6a, 0x83, 0xd3,
+	0xf8, 0xb0, 0x6c, 0x56, 0xd3, 0x77, 0xc7, 0xad, 0x45, 0xe7, 0x3b, 0xe9, 0xe4, 0x83, 0x40, 0xef,
+	0xc5, 0x49, 0x74, 0xf4, 0x0c, 0x12, 0x25, 0x33, 0x92, 0x93, 0x62, 0x50, 0x26, 0x4a, 0xd2, 0x0b,
+	0xe8, 0xa9, 0x80, 0xb5, 0xcf, 0x92, 0x3c, 0x2d, 0x06, 0x65, 0x07, 0x68, 0x0e, 0x43, 0x89, 0x5e,
+	0x38, 0x65, 0x83, 0x32, 0x3a, 0x4b, 0xa3, 0xfc, 0x90, 0x6a, 0x7d, 0xd6, 0x29, 0x81, 0xd9, 0xbf,
+	0x9c, 0x14, 0x49, 0xd9, 0x81, 0xad, 0x2f, 0x28, 0xcd, 0xa3, 0xaf, 0xb7, 0xf7, 0xed, 0xa8, 0xc9,
+	0x06, 0x46, 0x0f, 0xa6, 0x5e, 0x2a, 0x8d, 0x72, 0xb1, 0x56, 0xb6, 0x46, 0x1d, 0x8e, 0x3a, 0x5d,
+	0x42, 0xdf, 0x07, 0x1e, 0x9a, 0xb6, 0x54, 0xcb, 0x6d, 0x11, 0x9d, 0x01, 0x98, 0xf6, 0x08, 0xff,
+	0xa8, 0x7c, 0xc8, 0xd2, 0x3c, 0x2d, 0x86, 0xf3, 0x11, 0xdb, 0xaf, 0xc0, 0xe2, 0x85, 0xe5, 0x81,
+	0x66, 0xfe, 0x99, 0xc0, 0x79, 0x64, 0x9f, 0xb8, 0xe6, 0x15, 0xc6, 0xdf, 0xee, 0xe0, 0x7f, 0x85,
+	0xa1, 0x5b, 0xe3, 0x9a, 0x75, 0xc3, 0xb1, 0xdd, 0x70, 0x6c, 0x11, 0x9c, 0xd2, 0xd5, 0x2b, 0xdf,
+	0x34, 0x38, 0x3e, 0xca, 0xa6, 0xf7, 0x70, 0xe2, 0x91, 0x3b, 0xb1, 0x8e, 0xd0, 0xff, 0xd5, 0x3f,
+	0x23, 0x6d, 0x42, 0x63, 0x25, 0x0f, 0xb8, 0x4d, 0x38, 0xd2, 0x8c, 0x7f, 0xcc, 0x2c, 0x08, 0x7d,
+	0x86, 0x53, 0xeb, 0x8c, 0x40, 0xef, 0x7f, 0x55, 0xe2, 0xea, 0xe0, 0x83, 0xef, 0xcb, 0x17, 0x64,
+	0x46, 0x96, 0xfd, 0x68, 0xbb, 0xfd, 0x0a, 0x00, 0x00, 0xff, 0xff, 0x22, 0xf9, 0x23, 0x1b, 0x63,
+	0x02, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -135,6 +197,8 @@ const _ = grpc.SupportPackageIsVersion4
 type OrderManagementClient interface {
 	GetOrder(ctx context.Context, in *wrappers.StringValue, opts ...grpc.CallOption) (*Order, error)
 	SearchOrders(ctx context.Context, in *wrappers.StringValue, opts ...grpc.CallOption) (OrderManagement_SearchOrdersClient, error)
+	UpdateOrders(ctx context.Context, opts ...grpc.CallOption) (OrderManagement_UpdateOrdersClient, error)
+	ProcessOrders(ctx context.Context, opts ...grpc.CallOption) (OrderManagement_ProcessOrdersClient, error)
 }
 
 type orderManagementClient struct {
@@ -186,10 +250,77 @@ func (x *orderManagementSearchOrdersClient) Recv() (*Order, error) {
 	return m, nil
 }
 
+func (c *orderManagementClient) UpdateOrders(ctx context.Context, opts ...grpc.CallOption) (OrderManagement_UpdateOrdersClient, error) {
+	stream, err := c.cc.NewStream(ctx, &_OrderManagement_serviceDesc.Streams[1], "/ecommerce.OrderManagement/updateOrders", opts...)
+	if err != nil {
+		return nil, err
+	}
+	x := &orderManagementUpdateOrdersClient{stream}
+	return x, nil
+}
+
+type OrderManagement_UpdateOrdersClient interface {
+	Send(*Order) error
+	CloseAndRecv() (*wrappers.StringValue, error)
+	grpc.ClientStream
+}
+
+type orderManagementUpdateOrdersClient struct {
+	grpc.ClientStream
+}
+
+func (x *orderManagementUpdateOrdersClient) Send(m *Order) error {
+	return x.ClientStream.SendMsg(m)
+}
+
+func (x *orderManagementUpdateOrdersClient) CloseAndRecv() (*wrappers.StringValue, error) {
+	if err := x.ClientStream.CloseSend(); err != nil {
+		return nil, err
+	}
+	m := new(wrappers.StringValue)
+	if err := x.ClientStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+func (c *orderManagementClient) ProcessOrders(ctx context.Context, opts ...grpc.CallOption) (OrderManagement_ProcessOrdersClient, error) {
+	stream, err := c.cc.NewStream(ctx, &_OrderManagement_serviceDesc.Streams[2], "/ecommerce.OrderManagement/processOrders", opts...)
+	if err != nil {
+		return nil, err
+	}
+	x := &orderManagementProcessOrdersClient{stream}
+	return x, nil
+}
+
+type OrderManagement_ProcessOrdersClient interface {
+	Send(*wrappers.StringValue) error
+	Recv() (*CombinedShipment, error)
+	grpc.ClientStream
+}
+
+type orderManagementProcessOrdersClient struct {
+	grpc.ClientStream
+}
+
+func (x *orderManagementProcessOrdersClient) Send(m *wrappers.StringValue) error {
+	return x.ClientStream.SendMsg(m)
+}
+
+func (x *orderManagementProcessOrdersClient) Recv() (*CombinedShipment, error) {
+	m := new(CombinedShipment)
+	if err := x.ClientStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
 // OrderManagementServer is the server API for OrderManagement service.
 type OrderManagementServer interface {
 	GetOrder(context.Context, *wrappers.StringValue) (*Order, error)
 	SearchOrders(*wrappers.StringValue, OrderManagement_SearchOrdersServer) error
+	UpdateOrders(OrderManagement_UpdateOrdersServer) error
+	ProcessOrders(OrderManagement_ProcessOrdersServer) error
 }
 
 // UnimplementedOrderManagementServer can be embedded to have forward compatible implementations.
@@ -201,6 +332,12 @@ func (*UnimplementedOrderManagementServer) GetOrder(ctx context.Context, req *wr
 }
 func (*UnimplementedOrderManagementServer) SearchOrders(req *wrappers.StringValue, srv OrderManagement_SearchOrdersServer) error {
 	return status.Errorf(codes.Unimplemented, "method SearchOrders not implemented")
+}
+func (*UnimplementedOrderManagementServer) UpdateOrders(srv OrderManagement_UpdateOrdersServer) error {
+	return status.Errorf(codes.Unimplemented, "method UpdateOrders not implemented")
+}
+func (*UnimplementedOrderManagementServer) ProcessOrders(srv OrderManagement_ProcessOrdersServer) error {
+	return status.Errorf(codes.Unimplemented, "method ProcessOrders not implemented")
 }
 
 func RegisterOrderManagementServer(s *grpc.Server, srv OrderManagementServer) {
@@ -246,6 +383,58 @@ func (x *orderManagementSearchOrdersServer) Send(m *Order) error {
 	return x.ServerStream.SendMsg(m)
 }
 
+func _OrderManagement_UpdateOrders_Handler(srv interface{}, stream grpc.ServerStream) error {
+	return srv.(OrderManagementServer).UpdateOrders(&orderManagementUpdateOrdersServer{stream})
+}
+
+type OrderManagement_UpdateOrdersServer interface {
+	SendAndClose(*wrappers.StringValue) error
+	Recv() (*Order, error)
+	grpc.ServerStream
+}
+
+type orderManagementUpdateOrdersServer struct {
+	grpc.ServerStream
+}
+
+func (x *orderManagementUpdateOrdersServer) SendAndClose(m *wrappers.StringValue) error {
+	return x.ServerStream.SendMsg(m)
+}
+
+func (x *orderManagementUpdateOrdersServer) Recv() (*Order, error) {
+	m := new(Order)
+	if err := x.ServerStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+func _OrderManagement_ProcessOrders_Handler(srv interface{}, stream grpc.ServerStream) error {
+	return srv.(OrderManagementServer).ProcessOrders(&orderManagementProcessOrdersServer{stream})
+}
+
+type OrderManagement_ProcessOrdersServer interface {
+	Send(*CombinedShipment) error
+	Recv() (*wrappers.StringValue, error)
+	grpc.ServerStream
+}
+
+type orderManagementProcessOrdersServer struct {
+	grpc.ServerStream
+}
+
+func (x *orderManagementProcessOrdersServer) Send(m *CombinedShipment) error {
+	return x.ServerStream.SendMsg(m)
+}
+
+func (x *orderManagementProcessOrdersServer) Recv() (*wrappers.StringValue, error) {
+	m := new(wrappers.StringValue)
+	if err := x.ServerStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
 var _OrderManagement_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "ecommerce.OrderManagement",
 	HandlerType: (*OrderManagementServer)(nil),
@@ -260,6 +449,17 @@ var _OrderManagement_serviceDesc = grpc.ServiceDesc{
 			StreamName:    "searchOrders",
 			Handler:       _OrderManagement_SearchOrders_Handler,
 			ServerStreams: true,
+		},
+		{
+			StreamName:    "updateOrders",
+			Handler:       _OrderManagement_UpdateOrders_Handler,
+			ClientStreams: true,
+		},
+		{
+			StreamName:    "processOrders",
+			Handler:       _OrderManagement_ProcessOrders_Handler,
+			ServerStreams: true,
+			ClientStreams: true,
 		},
 	},
 	Metadata: "product_info.proto",
--- ch03/order-service/server/main.go
+++ ch03/order-service/server/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/golang/protobuf/ptypes/wrappers"
 	"google.golang.org/grpc"
+	"io"
 	"log"
 	"net"
 	pb "ordermgt/server/ecommerce"
@@ -13,12 +14,63 @@ import (
 
 const (
 	port = ":50051"
+	orderBatchSize = 3
 )
 
 var orderMap = make(map[string]pb.Order)
 
 type server struct {
 	orderMap map[string]*pb.Order
+}
+
+func (s *server) ProcessOrders(stream pb.OrderManagement_ProcessOrdersServer) error {
+	batchMarker := 1
+	var combinedShipmentMap = make(map[string]pb.CombinedShipment)
+	for {
+		orderId, err := stream.Recv()
+		log.Printf("Reading Proc order ; %s", orderId)
+		if err == io.EOF {
+			log.Printf("EOF : %s", orderId)
+			for _, shipment := range combinedShipmentMap {
+				if err := stream.Send(&shipment); err != nil {
+					return err
+				}
+			}
+			return nil
+		}
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+
+		destination := orderMap[orderId.GetValue()].Destination
+		shipment, found := combinedShipmentMap[destination]
+
+		if found {
+			ord := orderMap[orderId.GetValue()]
+			shipment.OrdersList = append(shipment.OrdersList, &ord)
+			combinedShipmentMap[destination] = shipment
+		} else {
+			comShip := pb.CombinedShipment{Id: "cmb - " + (orderMap[orderId.GetValue()].Description), Status: "Processed!"}
+			ord := orderMap[orderId.GetValue()]
+			comShip.OrdersList = append(shipment.OrdersList, &ord)
+			combinedShipmentMap[destination] = comShip
+			log.Print(len(comShip.OrdersList), comShip.GetId())
+		}
+
+		if batchMarker == orderBatchSize {
+			for _, comb := range combinedShipmentMap {
+				log.Printf("Shipping : %v -> %v", comb.Id, len(comb.OrdersList))
+				if err := stream.Send(&comb); err != nil {
+					return err
+				}
+			}
+			batchMarker = 0
+			combinedShipmentMap = make(map[string]pb.CombinedShipment)
+		} else {
+			batchMarker++
+		}
+	}
 }
 
 func (s *server) GetOrder(ctx context.Context, orderId *wrappers.StringValue) (*pb.Order, error) {
@@ -44,7 +96,22 @@ func (s *server) SearchOrders(searchQuery *wrappers.StringValue, strem pb.OrderM
 	return nil
 }
 
+func (s *server) UpdateOrders(stream pb.OrderManagement_UpdateOrdersServer) error {
+	ordersStr := "Update Order IDs : "
+	for {
+		order, err := stream.Recv() // 从客户端流中读取消息
+		if err == io.EOF {          // 检查流是否已经结束
+			return stream.SendAndClose(&wrappers.StringValue{Value: "Orders processed " + ordersStr}) // 服务端发送响应
+		}
+		orderMap[order.Id] = *order
+
+		log.Println("Order ID ", order.Id, ": Updated")
+		ordersStr += order.Id + ","
+	}
+}
+
 func main() {
+	initSampleData()
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
@@ -54,4 +121,12 @@ func main() {
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
+}
+
+func initSampleData() {
+	orderMap["102"] = pb.Order{Id: "102", Items: []string{"Google Pixel 3A", "Mac Book Pro"}, Destination: "Mountain View, CA", Price: 1800.00}
+	orderMap["103"] = pb.Order{Id: "103", Items: []string{"Apple Watch S4"}, Destination: "San Jose, CA", Price: 400.00}
+	orderMap["104"] = pb.Order{Id: "104", Items: []string{"Google Home Mini", "Google Nest Hub"}, Destination: "Mountain View, CA", Price: 400.00}
+	orderMap["105"] = pb.Order{Id: "105", Items: []string{"Amazon Echo"}, Destination: "San Jose, CA", Price: 30.00}
+	orderMap["106"] = pb.Order{Id: "106", Items: []string{"Amazon Echo", "Apple iPhone XS"}, Destination: "Mountain View, CA", Price: 300.00}
 }
